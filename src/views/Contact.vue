a8d8a719bf5583043f2ea4db8192176da52ebbe4
Enigmatic Aura <60955413+adicahyadir@users.noreply.github.com>
2025-01-03 15:17:37 +0700

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,11 +4,11 @@
       <div class="mt-6 text-center">
         <p>Follow us on:</p>
         <ul class="mt-4 space-y-2">
-          <li><a href="#" class="text-blue-500">GitHub</a></li>
+          <li><a href="https://github.com/adicahyadir" class="text-blue-500">GitHub</a></li>
           <li><a href="#" class="text-blue-500">Notion</a></li>
-          <li><a href="#" class="text-blue-500">GitBook</a></li>
-          <li><a href="#" class="text-blue-500">Twitter</a></li>
-          <li><a href="#" class="text-blue-500">Telegram</a></li>
+          <li><a href="https://enigmatics.gitbook.io/enigmatic-aura" class="text-blue-500">GitBook</a></li>
+          <li><a href="https://x.com/@enigmaticaura_" class="text-blue-500">Twitter</a></li>
+          <li><a href="https://t.me/enigmaticaura404" class="text-blue-500">Telegram</a></li>
         </ul>
       </div>
     </section>
